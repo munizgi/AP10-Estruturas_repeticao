d04a9fe17ca5147d3c05dd6c35e4f68c87830946
Giovanna Muniz <munz.giovanna01@gmail.com>
2026-04-02 15:54:18 -0300

    Questão 2 - Contador Positivos e Negativos

--- Exc10.sql
+++ Exc10.sql
@@ -14,3 +14,26 @@ BEGIN
     END LOOP;
 END; 
 $$
+
+
+-- QUESTÃO 2
+-- Escreva um programa que leia 6 números. Esses números serão apenas positivos ou negativos (desconsiderar valores nulos). Imprimir o total de números positivos.
+
+DO $$
+DECLARE
+    n INT;
+    contador INT := 0; -- conta quantas vezes o loop repete
+    cont_positivos INT := 0; -- conta quantos positivos saem
+BEGIN
+    LOOP
+        contador := contador + 1; --soma cada iteração 
+        n := fn_valor_aleatorio_entre(-50,50);
+        RAISE NOTICE 'Gerado: %', n;
+        IF n > 0 THEN
+            cont_positivos := cont_positivos + 1; --soma cada numero positivo
+        END IF;
+        EXIT WHEN contador = 6; -- encerra quando chega a seis iterações
+    END LOOP;
+    RAISE NOTICE 'Total positivos: %', cont_positivos;
+END;
+$$;
